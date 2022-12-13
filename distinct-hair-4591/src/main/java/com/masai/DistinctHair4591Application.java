4340bcef1b94cbbd11698e656db4d326a10c2f3a
piyushtyagi28@hotmail.com <piyushtyagi.1856@gmail.com>
2022-12-13 21:09:00 +0530

    day1Overview

---
+++ distinct-hair-4591/src/main/java/com/masai/DistinctHair4591Application.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistinctHair4591Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistinctHair4591Application.class, args);
+	}
+
+}
---
+++ distinct-hair-4591/src/test/java/com/masai/DistinctHair4591ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistinctHair4591ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
