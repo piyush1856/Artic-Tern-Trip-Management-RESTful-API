a71e91a9c7e850aaecf88a8cddca6acd1d7cb587
Shiv-96 <kshiv.dot@gmail.com>
2022-12-15 20:40:56 +0530

    Exception Part 2

---
+++ distinct-hair-4591/src/main/java/com/articTern/exceptions/TravelAgencyException.java
@@ -0,0 +1,16 @@
+package com.articTern.exceptions;
+
+public class TravelAgencyException extends Exception {
+	
+	public TravelAgencyException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public TravelAgencyException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+	
+	
+}
---
+++ distinct-hair-4591/src/main/java/com/articTern/exceptions/UserException.java
@@ -0,0 +1,16 @@
+package com.articTern.exceptions;
+
+public class UserException extends Exception {
+	
+	public UserException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public UserException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+	
+	
+}
